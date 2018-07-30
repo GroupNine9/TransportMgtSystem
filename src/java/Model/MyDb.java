1faae81a29b4f22314c112f238ca78bc0147e91e
grace <njorogegwanjiru@gmail.com>
2018-07-30 13:46:42 +0300

    Project Structure

---
+++ src/java/Model/MyDb.java
@@ -0,0 +1,30 @@
+package Model;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyDb {
+    Connection con;
+    String driverName = "com.mysql.jdbc.Driver";
+    String connectionURL = "jdbc:mysql://localhost:3306/transportmanagement";
+    String username = "root";
+    String password = "";
+    
+    public Connection getCon()
+    {
+        try
+        {
+            Class.forName(driverName);
+            con = DriverManager.getConnection(connectionURL,username, password);
+        }
+        catch (ClassNotFoundException | SQLException e) 
+        {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
